9b11df72e0c167600eb8500bfd3539a2ef448037
rebeca-august <rebeca.cazacu@gmail.com>
2021-01-12 18:52:38 +0000

    Finish App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from 'react'
+import questions from './data'
+import data from './data'
+import SingleQuestion from './Question'
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <SingleQuestion
+              id={question.id}
+              title={question.title}
+              info={question.info}
+            />
+          ))}
+        </section>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
